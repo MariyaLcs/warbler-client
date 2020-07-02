2fc4667ebbd6dd415a16c4891e4a7daf57b73f9c
Mariya <mariya.podosinova@gmail.com>
2020-07-02 13:54:33 +0100

    Logging out a User

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -2,8 +2,13 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import Logo from "../images/warbler-logo.png";
+import { logout } from "../store/actions/auth";
 
 class Navbar extends Component {
+  logout = (e) => {
+    e.preventDefault();
+    this.props.logout();
+  };
   render() {
     return (
       <nav className="navbar navbar-expand bg-dark">
@@ -13,16 +18,34 @@ class Navbar extends Component {
               <img src={Logo} alt="Home" />
             </Link>
           </div>
-          <ul className="nav navbar-nav navbar-right">
-            <li>
-              <i className="fas fa-user-plus mr-2 text-white"></i>
-              <Link to="/signup">Sign up</Link>
-            </li>
-            <li>
-              <i className="fa fa-beer mr-2 text-white"></i>
-              <Link to="/signin">Log In</Link>
-            </li>
-          </ul>
+          {this.props.currentUser.isAuthenticated ? (
+            <ul className="nav navbar-nav navbar-right">
+              <li>
+                <Link
+                  className="text-light"
+                  to={`/users/${this.props.currentUser.user.id}/messages/new`}
+                >
+                  New Message
+                </Link>
+              </li>
+              <li>
+                <a onClick={this.logout} className="text-light">
+                  Log out
+                </a>
+              </li>
+            </ul>
+          ) : (
+            <ul className="nav navbar-nav navbar-right">
+              <li>
+                <i className="fas fa-user-plus mr-2 text-white"></i>
+                <Link to="/signup">Sign up</Link>
+              </li>
+              <li>
+                <i className="fa fa-beer mr-2 text-white"></i>
+                <Link to="/signin">Log In</Link>
+              </li>
+            </ul>
+          )}
         </div>
       </nav>
     );
@@ -35,4 +58,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps, null)(Navbar);
+export default connect(mapStateToProps, { logout })(Navbar);
