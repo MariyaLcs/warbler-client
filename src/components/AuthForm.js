25d779c4ce8e73a424fe68c28cd925081d93ec4e
Mariya <mariya.podosinova@gmail.com>
2020-07-02 12:42:14 +0100

    Handling Error Messages with Redux

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -30,13 +30,27 @@ export default class AuthForm extends Component {
 
   render() {
     const { email, username, password, profileImageUrl } = this.state;
-    const { signUp, heading, buttonText } = this.props;
+    const {
+      signUp,
+      heading,
+      buttonText,
+      errors,
+      history,
+      removeError,
+    } = this.props;
+
+    history.listen(() => {
+      removeError();
+    });
     return (
       <div>
         <div className="row justify-content-md-center text-center">
           <div className="col-md-6">
             <form onSubmit={this.handleSubmit}>
               <h2>{heading}</h2>
+              {errors.message && (
+                <div className="alert alert-danger">{errors.message}</div>
+              )}
               <label htmlFor="email">Email:</label>
               <input
                 autoComplete="off"
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,6 @@
 import { apiCall } from "../../services/api";
 import { SET_CURRENT_USER } from "../actionsTypes";
+import { addError, removeError } from "./errors";
 
 export function setCurrentUser(user) {
   return {
@@ -10,14 +11,20 @@ export function setCurrentUser(user) {
 
 export function authUser(type, userData) {
   return (dispatch) => {
+    // wrap our thunk in a promise so we can wait for the API call
     return new Promise((resolve, reject) => {
-      return apiCall("post", `/api/auth/${type}`, userData).then(
-        ({ token, ...user }) => {
+      return apiCall("post", `/api/auth/${type}`, userData)
+        .then(({ token, ...user }) => {
           localStorage.setItem("jwtToken", token);
+          // setAuthorizationToken(token);
           dispatch(setCurrentUser(user));
+          dispatch(removeError());
           resolve(); // indicate that the API call succeeded
-        }
-      );
+        })
+        .catch((err) => {
+          dispatch(addError(err.message));
+          reject(); // indicate the API call failed
+        });
     });
   };
 }
