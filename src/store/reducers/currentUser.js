0b57796287a26f869b74b9480332ad6421d067bb
Mariya <mariya.podosinova@gmail.com>
2020-07-07 11:46:18 +0100

    fixed logout bug

--- src/store/reducers/currentUser.js
+++ src/store/reducers/currentUser.js
@@ -1,16 +1,19 @@
 import { SET_CURRENT_USER } from "../actionsTypes";
 
 const DEFAULT_STATE = {
-  isAuthenticated: false,
-  user: {},
+  isAuthenticated: false, // hopefully be true, when logged in
+  user: {}, // all the user info when logged in
 };
 
 export default (state = DEFAULT_STATE, action) => {
   switch (action.type) {
     case SET_CURRENT_USER:
-      //!! means Boolean()
-      return { isAuthenticated: !!Object.keys(action.user), user: action.user };
-
+      return {
+        //!! means Boolean()
+        // turn empty object into false or if there are keys, true
+        isAuthenticated: !!Object.keys(action.user).length,
+        user: action.user,
+      };
     default:
       return state;
   }
