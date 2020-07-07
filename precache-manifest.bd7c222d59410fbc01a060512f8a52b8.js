45f45e6fbc580c82a55c7d4261437ef88500b2cf
Mariya <mariya.podosinova@gmail.com>
2020-07-07 14:58:19 +0100

    Updates

---
+++ precache-manifest.bd7c222d59410fbc01a060512f8a52b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db96a2741548f1625f07ca926e072511",
+    "url": "/warbler-client/index.html"
+  },
+  {
+    "revision": "9b300ab94ff9a32e5e98",
+    "url": "/warbler-client/static/css/main.8c3e2431.chunk.css"
+  },
+  {
+    "revision": "b63d3b7f24f66ac4a398",
+    "url": "/warbler-client/static/js/2.51e19ab5.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/warbler-client/static/js/2.51e19ab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b300ab94ff9a32e5e98",
+    "url": "/warbler-client/static/js/main.6609bf2d.chunk.js"
+  },
+  {
+    "revision": "b45e056d8fb4f8b9eb6b",
+    "url": "/warbler-client/static/js/runtime-main.6406c426.js"
+  },
+  {
+    "revision": "25398cf74efabfc8f446dfcb6c2a6338",
+    "url": "/warbler-client/static/media/warbler-logo.25398cf7.png"
+  }
+]);
